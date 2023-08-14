1e3bc1c9c953aa5cc8acdbb0703a469601b7260d
Shashank Dhakate <shashankdkte@gmail.com>
2023-08-14 18:18:31 +0530

    Fetch menu Items and Interfaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+
 import { Header, Footer } from "./Components/Layout";
+import { menuItemModel } from './Intefaces';
 
 import './App.css';
 
 function App() {
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+
+  useEffect(() => {
+    fetch("https://redmangoapi.azurewebsites.net/api/MenuItem")
+      .then((response) => response.json())
+      .then(data => {
+        console.log(data)
+        setMenuItems(data.results);
+      })
+  },[])
   return (
     <div className="App">
       <Header />
