736e0be1a7814dc2d4bfc8808dc96cc88eb07eed
Shashank Dhakate <shashankdkte@gmail.com>
2023-08-14 17:57:56 +0530

    Header and Footer Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Header, Footer } from "./Components/Layout";
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App text-success">
-      App
+    <div className="App">
+      <Header />
+      MainBody
+      <Footer />
     </div>
   );
 }
